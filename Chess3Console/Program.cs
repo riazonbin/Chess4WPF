e1f47e89f9bb523274ccdfd2d966806be82e96a8
riazonbin <riazonbin@mail.ru>
2022-06-10 18:58:55 +0300

    Added console project (chess3)

---
+++ Chess3Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chess3Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
